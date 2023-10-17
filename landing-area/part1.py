ea64afdd2fda6de14591fc88e3f163ccfac42550
rivques <38469076+rivques@users.noreply.github.com>
2023-10-17 14:10:03 -0400

    comment

--- landing-area/part1.py
+++ landing-area/part1.py
@@ -1,25 +1,25 @@
 import time
 
-def validate_input(input_string: str):
+def validate_input(input_string: str): # return false on error or an array of the form [x, y] on success
     try:
         input_parts = input_string.split(",")
     except ValueError:
         return False
-    if len(input_parts) != 2:
+    if len(input_parts) != 2: # we expect an x and a y coordinate
         return False
     try:
-        result = [float(part) for part in input_parts]
+        result = [float(part) for part in input_parts] # turn the strings in input_parts into floats
         return result
-    except ValueError:
+    except ValueError: # if something wasn't a float
         return False
 
-def get_area(p1, p2, p3):
+def get_area(p1, p2, p3): # adapted from https://stackoverflow.com/questions/59597399
     area = 0.5 * (p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1] - p1[1]) + p3[0]
                   * (p1[1] - p2[1]))
     return abs(area)
 
 while True:
-    vertex_1 = validate_input(input("Please input three triangle vertices in x,y format. Vertex 1: "))
+    vertex_1 = validate_input(input("Please input three triangle vertices in x,y format. Vertex 1: ")) # this is a little bit repetitive but i want to use continue here
     if not vertex_1:
         print("That was not the proper format. Please try again.")
         continue

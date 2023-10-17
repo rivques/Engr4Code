1b9757bcb09e41a1c6a14a69fda3ae90bd82a956
rivques <38469076+rivques@users.noreply.github.com>
2023-10-17 14:02:05 -0400

    landing area part 1

---
+++ landing-area/part1.py
@@ -0,0 +1,36 @@
+import time
+
+def validate_input(input_string: str):
+    try:
+        input_parts = input_string.split(",")
+    except ValueError:
+        return False
+    if len(input_parts) != 2:
+        return False
+    try:
+        result = [float(part) for part in input_parts]
+        return result
+    except ValueError:
+        return False
+
+def get_area(p1, p2, p3):
+    area = 0.5 * (p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1] - p1[1]) + p3[0]
+                  * (p1[1] - p2[1]))
+    return abs(area)
+
+while True:
+    vertex_1 = validate_input(input("Please input three triangle vertices in x,y format. Vertex 1: "))
+    if not vertex_1:
+        print("That was not the proper format. Please try again.")
+        continue
+    vertex_2 = validate_input(input("Vertex 2: "))
+    if not vertex_2:
+        print("That was not the proper format. Please try again.")
+        continue
+    vertex_3 = validate_input(input("Vertex 3: "))
+    if not vertex_3:
+        print("That was not the proper format. Please try again.")
+        continue
+    area = get_area(vertex_1, vertex_2, vertex_3)
+    print(f"The area of the triangle with vertices ({vertex_1[0]},{vertex_1[1]}), ({vertex_2[0]},{vertex_2[1]}), ({vertex_3[0]},{vertex_3[1]}) is {area} square km.")
+

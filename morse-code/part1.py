25cfb8ce89c94adc8fddb55f339d6342428aaf65
rivques <38469076+rivques@users.noreply.github.com>
2023-10-31 13:05:54 -0400

    morse1

---
+++ morse-code/part1.py
@@ -0,0 +1,37 @@
+MORSE_CODE = { 'A':'.-', 'B':'-...',
+    'C':'-.-.', 'D':'-..', 'E':'.',
+    'F':'..-.', 'G':'--.', 'H':'....',
+    'I':'..', 'J':'.---', 'K':'-.-',
+    'L':'.-..', 'M':'--', 'N':'-.',
+    'O':'---', 'P':'.--.', 'Q':'--.-',
+    'R':'.-.', 'S':'...', 'T':'-',
+    'U':'..-', 'V':'...-', 'W':'.--',
+    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+    '1':'.----', '2':'..---', '3':'...--',
+    '4':'....-', '5':'.....', '6':'-....',
+    '7':'--...', '8':'---..', '9':'----.',
+    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+    '?':'..--..', '/':'-..-.', '-':'-....-',
+    '(':'-.--.', ')':'-.--.-'}
+
+while True:
+    user_input = input("Enter the string to translate, or type '-q' to quit. ")
+    user_input = user_input.upper()
+    if user_input == "-Q":
+        break
+    morse_translation = ""
+    translation_good = True
+    for letter in user_input:
+        if letter == " ":
+            morse_translation += "/"
+        else:
+            try:
+                morse_translation += MORSE_CODE[letter] + " "
+            except KeyError:
+                print(f"Unsupported character \"{letter}\" used. Please try again.")
+                translation_good = False
+                break
+    if translation_good:
+        print(morse_translation) 
+            
+print("Thanks for using the Morse Code Translator. Goodbye!")
